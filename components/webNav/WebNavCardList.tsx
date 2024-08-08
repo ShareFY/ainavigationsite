b7ff9eca0211685a21d056b331e914f13fa072be
GX <2031993325@qq.com>
2024-08-08 23:34:22 +0800

    fix build error

--- components/webNav/WebNavCardList.tsx
+++ components/webNav/WebNavCardList.tsx
@@ -1,9 +1,7 @@
 import type { WebNavigation } from '@/db/supabase/types';
-
 import WebNavCard from './WebNavCard';
 
 export default function WebNavCardList({ dataList }: { dataList: WebNavigation[] }) {
-
   return (
     <div className='grid grid-cols-2 gap-3 lg:grid-cols-4 lg:gap-4'>
       {dataList.map((item) => (
